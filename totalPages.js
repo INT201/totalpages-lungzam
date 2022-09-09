25757f4fafc1c06f85fc51e43895dc736a65b8c6
lungzam <88131334+lungzam@users.noreply.github.com>
2022-09-10 03:50:41 +0700

    dejai

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,13 @@
 const { template } = require('@babel/core')
+const { match } = require('assert')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if(arrayItems === null || arrayItems === undefined){
+    return undefined
+  }else if(rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0){
+    return 1
+  }else{
+    return Math.ceil(arrayItems.length/rowsPerPage)
+  }
 }
 module.exports = totalPages
